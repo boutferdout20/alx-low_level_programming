22e8b4f0520c7342a1eebc35507aeb32e69b3ccf
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-03-16 08:08:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non 0 otherwise
+ */
 int main(void)
 {
-/*Puts is designed to print strings to the console*/
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
