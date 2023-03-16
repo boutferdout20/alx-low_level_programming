39b77f16691e799e4e270adf7ad7a422ac3baea8
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-03-16 05:58:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-/Puts is designed to print strings to the console/
-puts("/"Programming is like building a multilingual puzzle");
+
+puts("\ "Programming is like building a multilingual puzzle");
 return (0);
 }
