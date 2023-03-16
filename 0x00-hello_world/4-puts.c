31235f70ffb8dd7f2baff1f89f7c48dad81bcd89
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-03-16 04:32:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main-prints a message to the console
- *Return :0 (Success)
+ * main - this is the main function
+ *
+ * Return: always 0
  */
+
+
+
+
 int main(void)
 {
-/Puts is designed to ptint strings to the console/
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle);
 return (0);
 }
