1b995ceaa14a2cf2ddb06ccd60e8fb806d806d23
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-03-30 03:12:29 -0700

    103-add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+int main(void);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
