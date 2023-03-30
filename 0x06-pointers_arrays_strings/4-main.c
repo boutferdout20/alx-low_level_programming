a869124856e101ae5d1161ae9398b13330be618c
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-03-30 02:18:16 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  * @a: an array of integers
@@ -31,9 +32,10 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-print_array(a, sizeof(a) / sizeof(int));
-reverse_array(a, sizeof(a) / sizeof(int));
-print_array(a, sizeof(a) / sizeof(int));
-return (0);
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	return (0);
 }
