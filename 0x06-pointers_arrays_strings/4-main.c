32b6e6bf90a6e6ccf86562fbcf5b037083418825
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-03-30 03:38:44 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -3,39 +3,15 @@
 
 /**
  * main - check the code
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
- */
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-	if (i != 0)
-	{
-	printf(", ");
-	}
-	printf("%d", a[i]);
-	i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-	
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
+char buffer[] = "This is a string!'\0'And this is the rest of the #buffer :)\1\2\3\4\5\6\7'#'cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56'#'pointersarefun'#'infernumisfun\n";
+
+printf(" % s \n", buffer);
+printf("---------------------------------\n");
+print_buffer(buffer, sizeof(buffer));
+return (0);
 }
