846b9cc44b260de1299e01c776668ea64e7fb719
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-05 06:53:30 -0700

    5-sqrt.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+int actual_sqrt_recursion(int n, int b);
+/**
+ * _sqrt_recursion - return natural square root of the number
+ * @n: number bach nhsbo
+ * Return: resultat square
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return(actual_sqrt_recursion(n, 0));
+}
+/**
+ * actual_sqrt_recursion - search for tha natural raqem f root
+ * @n: number bach nhsbo
+ * @b: iterator
+ * Return: resulat square
+ */
+int actual_sqrt_recursion(int n, int b)
+{
+if (b * b > n)
+{
+return (-1);
+}
+if (b * b == n)
+{
+return (b);
+}
+return (actual_sqrt_recursion(n, b + 1));
+}
