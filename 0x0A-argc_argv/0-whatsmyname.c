8096be6a8205da02b18e7887d3103e711feb4f81
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-08 15:50:47 -0700

    mynewnameis.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - write pro name
+ * @argv: liste
+ * @argc: chaine de charactere
+ * Return: dima 0
+ */
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
