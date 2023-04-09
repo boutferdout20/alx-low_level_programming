d4bcffa0594ce2cbd22cf8d3e7e1773bb4fe5f43
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-09 15:13:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - write
+ * Description: write the num
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
 int main(int argc, char const *argv[]);
 #endif
