76c5e4698481d0ea32e0f218113f757af406fd61
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-13 08:51:18 -0700

    101-mul.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,23 +13,17 @@ char *p;
 char *old_p;
 unsigned int a;
 if (new_size == old_size)
-{
 return (ptr);
-}
 if (new_size == 0 && ptr)
 {
 free(ptr);
 return (NULL);
 }
 if (!ptr)
-{
 return (malloc(new_size));
-}
 p = malloc(new_size);
 if (!p)
-{
 return (NULL);
-}
 old_p = ptr;
 if (new_size < old_size)
 {
