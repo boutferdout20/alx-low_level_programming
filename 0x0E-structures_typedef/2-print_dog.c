90f319e7d8fb68e7c87757c925bdc7a4c8207a34
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-17 06:24:41 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - ecrire une stricture
+ * @d: une stricture
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name:%s\nAga:%f\nOwner:%s\n", d->name, d->age, d->owner);
+}
