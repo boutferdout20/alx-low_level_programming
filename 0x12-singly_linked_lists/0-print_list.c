cdfabdad1403dc4683dc8bb30846ad947d9d601a
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-04-27 07:53:44 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include "lists.h"
+/**
+ * print_list - write the element
+ * @h: pointeur de rwayss
+ * Return: chiffre de nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t lin = 0;
+while (h != NULL)
+{
+if (h->str != NULL)
+printf("[%u] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
+h = h->next;
+lin++;
+}
+return (lin);
+}
