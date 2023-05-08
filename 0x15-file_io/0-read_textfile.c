245ce2a6e7aa29df23a0fa958d8226f76c358291
boutferdout20 <boutferdoutibtissam@gmail.com>
2023-05-08 10:13:25 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * read_textfile - lire text to print STDOUT
  * @filename: read text
  * @letters: les lettres
- * Return: 0 or NULL
+ * Return: 0 or faild or number
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
